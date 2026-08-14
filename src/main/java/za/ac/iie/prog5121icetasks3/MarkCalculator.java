66d4ca87e5a268b73e73227ee728e8015ebab838
Student <Student@LabVM2049939.aql4bnvrhije1fyosxkgrpdjne.jnbx.internal.cloudapp.net>
2026-08-14 16:38:33 +0200

    Complete ICE Task 3 unit tests

---
+++ src/main/java/za/ac/iie/prog5121icetasks3/MarkCalculator.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package za.ac.iie.prog5121icetasks3;
+
+/**
+ *
+ * @author Student
+ */
+public class MarkCalculator {
+
+   
+    /**
+     * Calculates the total of two marks.
+     *
+     * @param markOne first mark
+     * @param markTwo second mark
+     * @return the total of the two marks
+     */
+    public int calculateTotal(int markOne, int markTwo) {
+        return markOne + markTwo;
+    }
+
+    /**
+     * Calculates the average of two marks.
+     *
+     * @param markOne first mark
+     * @param markTwo second mark
+     * @return the average of the two marks
+     */
+    public double calculateAverage(int markOne, int markTwo) {
+        return (markOne + markTwo) / 2.0;
+    }
+}
